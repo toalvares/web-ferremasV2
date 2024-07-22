7047b39e17900927b4ae63453fd5e3ce40b3d54c
TomyPcHp <to.alvares@duocuc.cl>
2024-07-22 19:07:28 -0400

    API propia funcionando

--- src/pages/api/productos.json.ts
+++ src/pages/api/productos.json.ts
@@ -1,21 +1,15 @@
 import { db, Productos } from "astro:db";
 
-export async function get() {
+export async function GET() {
   try {
     const productos = await db.select().from(Productos);
-    return {
-      body: JSON.stringify(productos),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
+    return new Response(JSON.stringify(productos), {
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
-    return {
-      status: 500,
-      body: JSON.stringify({ error: 'Error al obtener los productos' }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
+    return new Response(
+      JSON.stringify({ error: "Error al obtener los productos" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
   }
 }
